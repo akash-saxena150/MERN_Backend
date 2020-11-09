d2fedac13786d7a27c52cf075b219f026b1c0820
aks-gsg <akash@getsetgo.fitness>
2020-11-09 11:34:42 +0530

    Code for fetching permissions

--- routes/permissions.js
+++ routes/permissions.js
@@ -4,11 +4,83 @@ const auth = require("../middleware/auth");
 const notAdmin = require("../middleware/notAdmin");
 const { getCollection, getCreatedData } = require("../services");
 //@route get api/permissions
+//@desc Show all permissions of requested user
+//@access Private
+
+Router.get("/:id", async (req, res) => {
+  try {
+    const winId = req.params.id;
+    const permissionRef = await getCollection("permissions")
+      .where("win_id", "==", winId)
+      .get();
+    let permissionData = [],
+      i = 0;
+    let permissions = {};
+    permissionRef.forEach(data => {
+      permissions = { ...data.data().permissions };
+    });
+    for (let domain in permissions) {
+      permissionData.push({});
+      let domainDataRef = await getCollection("domains")
+        .doc(domain)
+        .get();
+      let currDomain = domainDataRef.data();
+      permissionData[i]["domain_name"] = currDomain.name;
+      permissionData[i]["domain_id"] = currDomain.domain_id;
+      permissionData[i]["modules"] = permissionData[i]["modules"] || [];
+      for (let j = 0; permissions[domain][j]; j++) {
+        permissionData[i]["modules"][j] = {};
+        let moduleDataRef = await getCollection("modules")
+          .doc(permissions[domain][j])
+          .get();
+        let currModule = moduleDataRef.data();
+        console.log("CurrModule", currModule);
+        permissionData[i]["modules"][j].module_name = currModule.name;
+        permissionData[i]["modules"][j].module_id = currModule.module_id;
+      }
+      i++;
+    }
+    // permissionRef.forEach(userPermission => {
+    //   let permissions = { ...userPermission.data().permissions };
+
+    //   // Object.keys(permissions).map(async (domain, i) => {
+    //   //   permissionData.push({});
+    //   //   let domainDataRef = await getCollection("domains")
+    //   //     .doc(domain)
+    //   //     .get();
+    //   //   let currDomain = domainDataRef.data();
+    //   //   permissionData[i]["domain_name"] = currDomain.name;
+    //   //   permissionData[i]["domain_id"] = currDomain.domain_id;
+    //   //   permissionData[i]["modules"] = permissionData[i]["modules"] || [];
+    //   //   permissions[domain].map(async (module, j) => {
+    //   //     let moduleDataRef = await getCollection("modules")
+    //   //       .doc(module)
+    //   //       .get();
+    //   //     let currModule = moduleDataRef.data();
+    //   //     permissionData[i]["modules"].push({
+    //   //       module_name: currModule.name,
+    //   //       module_id: currModule.module_id
+    //   //     });
+    //   //     console.log("permissionData[i]", permissionData[i]);
+    //   //   });
+    //   //   // console.log("permissionData[i]", permissionData[i]);
+    //   // });
+    // });
+    console.log("permissionData", permissionData);
+    res.status(200).json({ permissionData });
+  } catch (err) {
+    console.log("Error", err);
+    res.status(500).send("Internal server error");
+  }
+});
+
+//@route get api/permissions
 //@desc Show all permissions of logged in user
 //@access Private
 
 Router.get("/", (req, res) => {
-  res.send("Permissions route loaded");
+  console.log("req.params.id");
+  res.send("Worked!");
 });
 
 //@route post api/permissions
@@ -22,14 +94,12 @@ Router.post("/", auth, notAdmin, async (req, res) => {
     //start id generation
     //Check if duplicate id is present with win_id
     let permission_id = "";
-    console.log("Permission id starts");
     const getDuplicatePermission = await permissionColRef
-      .where("win_id", "==", req.body.winId)
+      .where("win_id", "==", req.body.win_id)
       .get();
     if (!getDuplicatePermission.empty) {
       permission_id = getDuplicatePermission.doc().permission_id;
     }
-    console.log("Reached till here");
     if (!permission_id) {
       const permissionsRef = await permissionColRef.get();
       let numPermissions = 0;
